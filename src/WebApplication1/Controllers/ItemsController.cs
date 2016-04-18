7e415678727aff3100518b87cb404b7f5d8e781e
Will and Lawton <lawton.a.browning@gmail.com>
2016-04-18 10:44:13 -0700

    Add functionality to create and update tasks.

--- src/WebApplication1/Controllers/ItemsController.cs
+++ src/WebApplication1/Controllers/ItemsController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using WebApplication1.Models;
+using Microsoft.Data.Entity;
+
 
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
@@ -22,6 +24,30 @@ namespace WebApplication1.Controllers
         {
             var thisItem = db.Items.FirstOrDefault(x => x.ItemId == id);
             return View(thisItem);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            db.Items.Add(item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(x => x.ItemId == id);
+            return View(thisItem);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Item item)
+        {
+            db.Entry(item).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
