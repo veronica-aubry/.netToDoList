845926fba5e83902d6426cf465c3077161121998
Will and Lawton <lawton.a.browning@gmail.com>
2016-04-18 11:08:11 -0700

    Add ability to delete, and general layout.

--- src/WebApplication1/Controllers/ItemsController.cs
+++ src/WebApplication1/Controllers/ItemsController.cs
@@ -6,10 +6,6 @@ using Microsoft.AspNet.Mvc;
 using WebApplication1.Models;
 using Microsoft.Data.Entity;
 
-
-
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace WebApplication1.Controllers
 {
     public class ItemsController : Controller
@@ -48,6 +44,19 @@ namespace WebApplication1.Controllers
             db.Entry(item).State = EntityState.Modified;
             db.SaveChanges();
             return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(x => x.ItemId == id);
+            return View(thisItem);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var thisItem = db.Items.FirstOrDefault(x => x.ItemId == id);
+            db.Items.Remove(thisItem);
+            db.SaveChanges();
+            return RedirectToAction("index");
         }
     }
 }
